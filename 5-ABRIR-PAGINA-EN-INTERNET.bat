fc2573d9c185491d2b05c55139f76b3c0daae3ca
avantarcorporativo-al <avantarcorporativo@gmail.com>
2026-07-26 05:52:30 -0600

    Limpieza de accesos directos de internet y consolidacion de URL activa

--- 5-ABRIR-PAGINA-EN-INTERNET.bat
+++ 5-ABRIR-PAGINA-EN-INTERNET.bat
@@ -1,10 +1,10 @@
 @echo off
-title ABRIR PAGINA WHATSAPP EN INTERNET (FIREBASE HOSTING)
+title ABRIR PAGINA WHATSAPP CLON EN INTERNET (FIREBASE HOSTING)
 color 0B
 cls
 echo =======================================================
-echo   ABRIENDO PAGINA WHATSAPP DE INTERNET EN TU NAVEGADOR
+echo   ABRIENDO PAGINA WHATSAPP CLON DE INTERNET EN TU NAVEGADOR
 echo =======================================================
 echo.
-start https://restauran-editable-premiun.web.app/pagina_agente_universal.html
+start https://app-web-cambiar-imagenes-texto.web.app
 exit
